4fcaf8609ad5d17b2f681a95b86859a526451cca
Muhammad Azamuddin <mas.azamuddin@gmail.com>
2021-07-29 17:21:56 +0700

    chore: add dotenv

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const dotenv = require('dotenv');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+dotenv.config({path: './.env'});
 
 var app = express();
 
